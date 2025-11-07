ecd0997a8d3a2293ac4ea173b86dee3b2586c0ec
pascalspencer <pascalouma7480@gmail.com>
2025-11-07 13:18:23 +0300

    new changes

--- public/js/authConfig.mjs
+++ public/js/authConfig.mjs
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     APP_ID: 108991,
-    REDIRECT_URI: 'https://tradealgopro.vercel.app/deriv-oauth.html',
+    REDIRECT_URI: 'https://tradealgopro.vercel.app/',
     OAUTH_URL: 'https://oauth.deriv.com/oauth2/authorize',
     WS_ENDPOINT: 'wss://ws.binaryws.com/websockets/v3',
     APP_ORIGIN: 'https://tradealgopro.vercel.app'
