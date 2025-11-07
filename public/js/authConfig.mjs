50c0b67bb0e3109f7000801dbf99cc5128bbd75d
pascalspencer <pascalouma7480@gmail.com>
2025-11-07 13:30:04 +0300

    deriv endpoint update

--- public/js/authConfig.mjs
+++ public/js/authConfig.mjs
@@ -2,6 +2,6 @@ export const AUTH_CONFIG = {
     APP_ID: 108991,
     REDIRECT_URI: 'https://tradealgopro.vercel.app/',
     OAUTH_URL: 'https://oauth.deriv.com/oauth2/authorize',
-    WS_ENDPOINT: 'wss://ws.binaryws.com/websockets/v3',
+    WS_ENDPOINT: 'wss://ws.derivws.com/websockets/v3',
     APP_ORIGIN: 'https://tradealgopro.vercel.app'
 };
